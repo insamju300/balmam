0d9cdb209c56ad60bbd505651f134b5abe64ac58
insamju300 <insamju300@gmail.com>
2024-02-27 17:51:24 +0900

    회원 가입기능 중간저장

---
+++ src/main/resources/static/js/common.js
@@ -0,0 +1,29 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary_hard: "#5B9CB3",
+        primary: "#7EC8E3",
+        primary_soft: "#A3D4E9",
+        secondary_hard: "#F98816",
+        secondary: "#F2A65A",
+        secondary_soft: "#F7B98C",
+        accent_hard: "#E5706A",
+        accent: "#FF9A8D",
+        accent_soft: "#FFB3AB",
+        neutral_hard: "#003D5B",
+        neutral: "#005377",
+        neutral_soft: "#336B8A",
+        bg_main_hard: "#D9C59A",
+        bg_main: "#F4E8C1",
+        bg_main_soft: "#FBF3E0",
+        base_white: "#FFFFF0",
+        base_white_hard: "#E6E6D1",
+        base_white_soft: "#FFFDF5",
+      },
+    },
+  },
+};
+
+
+
