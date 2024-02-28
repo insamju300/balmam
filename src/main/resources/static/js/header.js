b6a4928d0a6717e6490ce461cd2a60810af0ec0e
insamju300 <insamju300@gmail.com>
2024-02-28 17:54:53 +0900

    반응형 + 회원정보 작업중

--- src/main/resources/static/js/header.js
+++ src/main/resources/static/js/header.js
@@ -1,14 +1,15 @@
 function showFullScreenMenu() {
   $("#full_size_slide_menu").addClass("top-0");
   $("#full_size_slide_menu").removeClass("-top-full");
+  $("#full_size_slide_menu").removeClass("-translate-y-80");
 
   anime({
     targets: "path",
     strokeDashoffset: [anime.setDashoffset, 0],
     easing: "easeInOutSine",
-    duration: 600,
+    duration: 300,
     delay: function (el, i) {
-      return i * 200;
+      return i * 100;
     },
     direction: "nomal",
     loop: false,
@@ -18,6 +19,8 @@ function showFullScreenMenu() {
 function hideFullScreenMenu(e) {
   if (e.target.tagName === "DIV") {
     $("#full_size_slide_menu").addClass("-top-full");
+    $("#full_size_slide_menu").addClass("-translate-y-80");
+
     $("#full_size_slide_menu").removeClass("top-0");
   }
 }
@@ -28,9 +31,9 @@ function drowPath(ele) {
     targets: path,
     strokeDashoffset: [anime.setDashoffset, 0],
     easing: "easeInOutSine",
-    duration: 1200,
+    duration: 500,
     delay: function (el, i) {
-      return i * 300;
+      return i * 200;
     },
     direction: "nomal",
     loop: false,
@@ -42,3 +45,14 @@ function commonsAlert(message = "", title = "") {
   $("#coommons_alert_modal h3").html(title);
   document.getElementById("coommons_alert_modal").showModal();
 }
+
+window.addEventListener("resize", function() {
+  let headerClientHeight = document.getElementById("navbar").offsetHeight;
+  $("#ghost_header").height(headerClientHeight);
+})
+
+$(window).on('load',function () {
+  let headerClientHeight = document.getElementById("navbar").offsetHeight;
+  $("#ghost_header").height(headerClientHeight);
+ 
+});
