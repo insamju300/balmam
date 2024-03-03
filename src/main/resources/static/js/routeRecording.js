35834e5e46cdaa145090d0c5480e2b44e1d2a31f
insamju300 <insamju300@gmail.com>
2024-03-04 01:09:55 +0900

    오래 머문 도시 리스트

--- src/main/resources/static/js/routeRecording.js
+++ src/main/resources/static/js/routeRecording.js
@@ -1,34 +1,33 @@
 let map;
 let marker;
+let geocoder;
 let lastUpdateTime;
 const intervalGap = 1000;
 let pathLine;
-let pathCoordinates=[];
-let pauseCount=0;
-let maxPauseCount=10;
-const pathLines=[];
-const pathCoordinatesGroups=[];
+let pathCoordinates = [];
+let pauseCount = 0;
+let maxPauseCount = 10;
+const pathLines = [];
+const pathCoordinatesGroups = [];
 let watchId;
 let intervalId;
 let intervalIdForTimeCheck;
 
-let timestampInHours = 1000*60*60;
-let timestampInMinutes = 1000*60;
+let timestampInHours = 1000 * 60 * 60;
+let timestampInMinutes = 1000 * 60;
 let timestampInSeconds = 1000;
 
 let recodingStartTime;
 let recodingEndTime;
-let recodingLimitTime = 3*timestampInMinutes;
+let recodingLimitTime = 3 * timestampInMinutes;
 
-let puseTime=null;
-let puseTimes=[];
+let puseTime = null;
+let puseTimes = [];
 
-
-
-
-
-
-
+let stayedCities = new Map();
+let currentStayedCityName;
+let currentStayedCityStartTime;
+let currentStayedCityEndTime;
 
 
 
@@ -37,9 +36,11 @@ async function initMap() {
   recodingStartTime = Date.now();
   const { Map } = await google.maps.importLibrary("maps");
   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+  const {Geocoder} = await google.maps.importLibrary("geocoding")
   const balmami = document.createElement("img");
   balmami.src = "/images/character/balmami.png";
-  balmami.style.height = "3rem";
+  balmami.classList.add("h-16");
+  balmami.classList.add("drop-shadow-lg");
 
   if (!navigator.geolocation) {
     handleLocationError(false, map.getCenter());
@@ -61,6 +62,30 @@ async function initMap() {
         content: balmami,
       });
 
+      geocoder = new google.maps.Geocoder();
+
+      geocoder.geocode({ location: userLocation }, (results, status) => {
+        if (status === google.maps.GeocoderStatus.OK) {
+          if (results[0]) {
+            // 결과에서 도시 이름을 찾습니다.
+            const addressComponents = results[0].address_components;
+            const cityComponent = addressComponents.find((component) =>
+            component.types.includes("administrative_area_level_1") &&
+            component.types.includes("political")
+            );
+            const cityName = cityComponent ? cityComponent.long_name : null;
+
+            if (cityName) {
+              currentStayedCityName = cityComponent.long_name;
+              currentStayedCityStartTime = Date.now();
+              console.log(currentStayedCityName);
+            }
+          } else {
+            console.log("Geocoder 실패: " + status);
+          }
+        }
+      });
+
       creatNewPathLine(userLocation);
       lastUpdateTime = Date.now();
     },
@@ -69,12 +94,9 @@ async function initMap() {
     }
   );
 
- 
-
-
   watchId = navigator.geolocation.watchPosition(
-    function(position){
-    successWatch(position);
+    function (position) {
+      successWatch(position);
     },
     function () {
       handleLocationError(true);
@@ -82,12 +104,20 @@ async function initMap() {
   );
 
   intervalIdForTimeCheck = setInterval(() => {
-    if(Date.now()-recodingStartTime-getTotalPuseTime()>recodingLimitTime){
-      commonsAlert(`최대 실녹화 시간(${Math.floor(recodingLimitTime/timestampInMinutes)}분)까지 녹화되어 다음 단계로 넘어갑니다.`, "녹화 종료 알림");
+    if (
+      Date.now() - recodingStartTime - getTotalPuseTime() >
+      recodingLimitTime
+    ) {
+      commonsAlert(
+        `최대 실녹화 시간(${Math.floor(
+          recodingLimitTime / timestampInMinutes
+        )}분)까지 녹화되어 다음 단계로 넘어갑니다.`,
+        "녹화 종료 알림"
+      );
       stopTraceRecoding();
       return;
     }
-  }, 1000); 
+  }, 1000);
 }
 
 function handleLocationError(browserHasGeolocation) {
@@ -100,17 +130,15 @@ function handleLocationError(browserHasGeolocation) {
 
 initMap();
 
-function resumeTraceRecoding(){
-
+function resumeTraceRecoding() {
   puseTime.endTime = Date.now();
   puseTimes.push(puseTime);
-  puseTime= null;
-
+  puseTime = null;
 
   navigator.geolocation.getCurrentPosition(
     function (position) {
       const userLocation = getUserLocation(position);
-      creatNewPathLine(userLocation)
+      creatNewPathLine(userLocation);
       lastUpdateTime = Date.now();
     },
     function () {
@@ -118,52 +146,48 @@ function resumeTraceRecoding(){
     }
   );
 
-  
-
   clearInterval(intervalId);
 
   watchId = navigator.geolocation.watchPosition(
-    function(position){
+    function (position) {
       successWatch(position);
-      },
+    },
     function () {
       handleLocationError(true);
     }
   );
 
-
   pathLine.setMap(map);
-    $("#trace_recoding_resume_btn").hide();
-    $("#trace_recoding_pause_btn").show();
+  $("#trace_recoding_resume_btn").hide();
+  $("#trace_recoding_pause_btn").show();
 }
 
-function pauseTraceRecoding(){
-  if(pauseCount>=maxPauseCount){
-    commonsAlert(`최대 일시정지 횟수(${maxPauseCount}회)를 초과하여 더 일시정지 하실 수 없습니다.`, "최대 일시정지 횟수 추가");
+function pauseTraceRecoding() {
+  if (pauseCount >= maxPauseCount) {
+    commonsAlert(
+      `최대 일시정지 횟수(${maxPauseCount}회)를 초과하여 더 일시정지 하실 수 없습니다.`,
+      "최대 일시정지 횟수 추가"
+    );
     return;
   }
   pathLines.push(pathLine);
-  puseTime=new Object();
+  puseTime = new Object();
   puseTime.startTime = Date.now();
- 
 
-  pathCoordinates=[];
-
-
+  pathCoordinates = [];
 
   navigator.geolocation.clearWatch(watchId);
 
   intervalId = setInterval(() => {
     navigator.geolocation.getCurrentPosition(
       (position) => {
-
         const userLocation = getUserLocation(position);
         // 위치 정보가 성공적으로 가져와졌을 때의 처리
-              // //사용자의 현재 위치로 마커 업데이트
-      marker.position = userLocation;
+        // //사용자의 현재 위치로 마커 업데이트
+        marker.position = userLocation;
 
-      // 지도의 중앙을 사용자의 현재 위치로 설정
-      map.setCenter(userLocation);
+        // 지도의 중앙을 사용자의 현재 위치로 설정
+        map.setCenter(userLocation);
       },
       (error) => {
         // 위치 정보를 가져오는데 실패했을 때의 처리
@@ -172,30 +196,50 @@ function pauseTraceRecoding(){
     );
   }, 1000); // 1초마다 getCurrentPosition을 호출
 
-
-
   $("#trace_recoding_pause_btn").hide();
   $("#trace_recoding_resume_btn").show();
   pauseCount++;
 }
 
-function stopTraceRecoding(){
+function stopTraceRecoding() {
   clearInterval(intervalId);
   clearInterval(intervalIdForTimeCheck);
   navigator.geolocation.clearWatch(watchId);
-  
-  if(puseTime && !puseTime.endTime){
+
+  if (puseTime && !puseTime.endTime) {
     puseTime.endTime = Date.now();
     puseTimes.push(puseTime);
   }
 
+  if (currentStayedCityName && !currentStayedCityEndTime) {
+    currentStayedCityEndTime = Date.now();
+    let stayedCity = {
+      name: currentStayedCityName,
+      stayedTime: currentStayedCityEndTime - currentStayedCityStartTime,
+    };
+    if (stayedCities.has(currentStayedCityName)) {
+      let existingStayedTime = stayedCities.get(currentStayedCityName);
+      stayedCities.set(
+        currentStayedCityName,
+        currentStayedCityEndTime -
+          currentStayedCityStartTime +
+          existingStayedTime
+      );
+    } else {
+      stayedCities.set(
+        currentStayedCityName,
+        currentStayedCityEndTime - currentStayedCityStartTime
+      );
+    }
+  }
+
   recodingEndTime = Date.now();
   let totalRecodingTime = recodingEndTime - recodingStartTime;
-  let hours = Math.floor(totalRecodingTime/timestampInHours);
+  let hours = Math.floor(totalRecodingTime / timestampInHours);
   totalRecodingTime %= timestampInHours;
-  let minutes = Math.floor(totalRecodingTime/timestampInMinutes);
+  let minutes = Math.floor(totalRecodingTime / timestampInMinutes);
   totalRecodingTime %= timestampInMinutes;
-  let seconds = Math.floor(totalRecodingTime/timestampInSeconds);
+  let seconds = Math.floor(totalRecodingTime / timestampInSeconds);
   console.log("전체 레코딩 시간");
   console.log(hours + ":" + minutes + ":" + seconds);
   console.log("녹화된 경로");
@@ -204,32 +248,28 @@ function stopTraceRecoding(){
   console.log(puseTimes);
   console.log("전체 일시정지 시간");
   console.log(getTotalPuseTime());
+  console.log("머문 도시 리스트");
+  console.log(stayedCities);
 
-  const date = new Date((recodingEndTime-recodingStartTime) * 1000);
+  const date = new Date((recodingEndTime - recodingStartTime) * 1000);
   alert(date);
-
 }
 
-
-
-function creatNewPathLine(userLocation){
-  
+function creatNewPathLine(userLocation) {
   const tmpPathCoordinates = [userLocation];
   pathCoordinatesGroups.push(tmpPathCoordinates);
-  pathCoordinates=tmpPathCoordinates;
+  pathCoordinates = tmpPathCoordinates;
 
   pathLine = new google.maps.Polyline({
     path: pathCoordinates,
-    strokeColor: '#E07A5F',
+    strokeColor: "#E07A5F",
     strokeOpacity: 0.7,
     strokeWeight: 2,
   });
   pathLine.setMap(map);
 }
 
-
-function getUserLocation(position){
-  
+function getUserLocation(position) {
   const userLocation = {
     lat: position.coords.latitude,
     lng: position.coords.longitude,
@@ -239,40 +279,87 @@ function getUserLocation(position){
   return userLocation;
 }
 
-function successWatch(position){
+function successWatch(position) {
+  //const {PlacesService} = await google.maps.importLibrary("places");
+  //const service = new google.maps.places.PlacesService(map);
+  //https://developer.mozilla.org/ko/docs/Web/API/Geolocation/watchPosition
 
-    //https://developer.mozilla.org/ko/docs/Web/API/Geolocation/watchPosition
+  let now = Date.now();
 
-    let now = Date.now();
+  if (lastUpdateTime && now - lastUpdateTime < intervalGap) {
+    return;
+  }
+  lastUpdateTime = now;
 
-    if (
-      lastUpdateTime &&
-      now - lastUpdateTime < intervalGap
-    ) {
-      return;
+  const userLocation = getUserLocation(position);
+  //로직 바꿀것.
+  geocoder.geocode({ location: userLocation }, (results, status) => {
+    if (status === google.maps.GeocoderStatus.OK) {
+      if (results[0]) {
+        // 결과에서 도시 이름을 찾습니다.
+        const addressComponents = results[0].address_components;
+        const cityComponent = addressComponents.find((component) =>
+          component.types.includes("administrative_area_level_1") &&
+          component.types.includes("political")
+        );
+        const cityName = cityComponent ? cityComponent.long_name : null;
+        console.log(addressComponents);
+
+        if (cityName && cityName !== currentStayedCityName) {
+          if (!currentStayedCityName) {
+            currentStayedCityName = cityComponent.long_name;
+            currentStayedCityStartTime = Date.now();
+          } else {
+            currentStayedCityEndTime = Date.now();
+            let stayedCity = {
+              name: currentStayedCityName,
+              stayedTime: currentStayedCityEndTime - currentStayedCityStartTime,
+            };
+            if (stayedCities.has(currentStayedCityName)) {
+              let existingStayedTime = stayedCities.get(currentStayedCityName);
+              stayedCities.set(
+                currentStayedCityName,
+                currentStayedCityEndTime -
+                  currentStayedCityStartTime +
+                  existingStayedTime
+              );
+            } else {
+              stayedCities.set(
+                currentStayedCityName,
+                currentStayedCityEndTime - currentStayedCityStartTime
+              );
+            }
+            currentStayedCityName = cityName;
+            currentStayedCityStartTime = Date.now();
+            currentStayedCityEndTime = null;
+          }
+        }
+      } else {
+        console.log("Geocoder 실패: " + status);
+      }
     }
-    lastUpdateTime = now;
+  });
 
-    const userLocation = getUserLocation(position);
+  console.log(currentStayedCityName);
 
-    //사용자의 현재 위치로 마커 업데이트
-    marker.position = userLocation;
-    
-    //선 그리기
-    pathCoordinates.push(userLocation);
-    pathLine.setPath(pathCoordinates);
-    map.panTo(userLocation);
+  //사용자의 현재 위치로 마커 업데이트
+  marker.position = userLocation;
 
-    // 지도의 중앙을 사용자의 현재 위치로 설정
-    map.setCenter(userLocation);
-  
+  //선 그리기
+  pathCoordinates.push(userLocation);
+  pathLine.setPath(pathCoordinates);
+  map.panTo(userLocation);
+
+  // 지도의 중앙을 사용자의 현재 위치로 설정
+  map.setCenter(userLocation);
 }
 
-function getTotalPuseTime(){
+function getTotalPuseTime() {
   const initialValue = 0;
   const totalPuseTime = puseTimes.reduce(
-    (accumulator, puseTime) => accumulator + (puseTime.endTime - puseTime.startTime),
-  initialValue,
+    (accumulator, puseTime) =>
+      accumulator + (puseTime.endTime - puseTime.startTime),
+    initialValue
   );
 
   return totalPuseTime;
